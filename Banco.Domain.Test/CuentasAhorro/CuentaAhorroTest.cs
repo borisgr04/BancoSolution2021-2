e8a6da447007d19bd7318764372bc4924bfe6c48
Boris Gonzalez <bgonzalez@byasystems.com.co>
2021-09-16 09:55:18 -0500

    se confirma avnaces del desarrollo de cuenta ahorro y corriente

--- Banco.Domain.Test/CuentasAhorro/CuentaAhorroTest.cs
+++ Banco.Domain.Test/CuentasAhorro/CuentaAhorroTest.cs
@@ -19,10 +19,6 @@ namespace Banco.Domain.Test.CuentasAhorro
         Cuando Va a consignar un valor -1
         Entonces El sistema presentará el mensaje. “El valor a consignar es incorrecto”
          */
-        /*
-         ENTITY=> CUENTA AHORRO => AGREGADO ROOT
-               => MOVIMIENTOS 
-         */
         [Test]
         public void NoPuedeConsignarValorDeMenosUno()
         {
@@ -57,54 +53,5 @@ namespace Banco.Domain.Test.CuentasAhorro
             Assert.AreEqual("Su Nuevo Saldo es de $ 50.000,00 pesos m/c", respuesta);
         }
     }
-    //Agregado
-    internal class CuentaAhorro
-    {
-        public string Numero { get; private set; }//encapsulamiento // guardar la integridad
-        public string Nombre { get; private set; }
-        public decimal Saldo { get; private set; }
-
-        private List<Movimiento> _movimientos;
-
-        public IReadOnlyCollection<Movimiento> Movimientos => _movimientos.AsReadOnly();
-
-        public CuentaAhorro(string numero, string nombre)
-        {
-            Numero = numero;
-            Nombre = nombre;
-            _movimientos = new List<Movimiento>();
-        }
-
-        internal string Consignar(decimal valorConsignacion, DateTime fecha)
-        {
-            if (valorConsignacion < 0)
-            {
-                return "El valor a consignar es incorrecto";
-            }
-            if (!_movimientos.Any() && valorConsignacion >= 50000)
-            {
-                _movimientos.Add(new Movimiento(cuentaAhorro:this, fecha: fecha,tipo: "CONSIGNACION", valor:valorConsignacion));
-                Saldo += valorConsignacion;
-
-                return $"Su Nuevo Saldo es de {Saldo:c2} pesos m/c";
-            }
-            throw new NotImplementedException();
-        }
-    }
-
-    internal class Movimiento
-    {
-        public Movimiento(CuentaAhorro cuentaAhorro, DateTime fecha, string tipo, decimal valor)
-        {
-            CuentaAhorro = cuentaAhorro;
-            Fecha = fecha;
-            Tipo = tipo;
-            Valor = valor;
-        }
-
-        public CuentaAhorro CuentaAhorro { get; private set; }
-        public DateTime Fecha { get; private set; }
-        public string Tipo { get; private set; }
-        public decimal Valor { get; private set; }
-    }
+      
 }
---
+++ Banco.Domain/CuentaAhorro.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco.Domain
+{
+    public class CuentaAhorro
+    {
+        public string Numero { get; private set; }//encapsulamiento // guardar la integridad
+        public string Nombre { get; private set; }
+        public decimal Saldo { get; private set; }
+
+        private List<Movimiento> _movimientos;
+
+        public IReadOnlyCollection<Movimiento> Movimientos => _movimientos.AsReadOnly();
+
+        public CuentaAhorro(string numero, string nombre)
+        {
+            Numero = numero;
+            Nombre = nombre;
+            _movimientos = new List<Movimiento>();
+        }
+
+        public string Consignar(decimal valorConsignacion, DateTime fecha)
+        {
+            if (valorConsignacion < 0)
+            {
+                return "El valor a consignar es incorrecto";
+            }
+            if (!_movimientos.Any() && valorConsignacion >= 50000)
+            {
+                _movimientos.Add(new Movimiento(cuentaAhorro: this, fecha: fecha, tipo: "CONSIGNACION", valor: valorConsignacion));
+                Saldo += valorConsignacion;
+
+                return $"Su Nuevo Saldo es de {Saldo:c2} pesos m/c";
+            }
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Movimiento
+    {
+        public Movimiento(CuentaAhorro cuentaAhorro, DateTime fecha, string tipo, decimal valor)
+        {
+            CuentaAhorro = cuentaAhorro;
+            Fecha = fecha;
+            Tipo = tipo;
+            Valor = valor;
+        }
+
+        public CuentaAhorro CuentaAhorro { get; private set; }
+        public DateTime Fecha { get; private set; }
+        public string Tipo { get; private set; }
+        public decimal Valor { get; private set; }
+    }
+}
